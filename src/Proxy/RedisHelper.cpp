b7485a73db711a87388c06f7e3e3c83b07bbc56d
coolpenguin37 <coolpenguin37@gmail.com>
2015-10-15 23:54:00 +0800

    RedisHelper

--- src/Proxy/RedisHelper.cpp
+++ src/Proxy/RedisHelper.cpp
@@ -1,13 +1,13 @@
 #include "RedisHelper.h"
 
-
+#include <redis3m/redis3m.hpp>
 #include <string>
 #include <string.h>
 #include <iostream>
 #include <stdint.h>
 
-using namespace std;
-
+using namespace redis3m;
+connection::ptr_t c;
 RedisHelper::RedisHelper()
 {
 }
@@ -20,7 +20,7 @@ RedisHelper::~RedisHelper()
 
 void RedisHelper::Open()
 {
-
+	c = connection::create();
 }
 
 void RedisHelper::Close()
@@ -30,12 +30,33 @@ void RedisHelper::Close()
 
 uint32_t RedisHelper::Get(char *pKey, uint32_t uiKeyLen, char *pOut, uint32_t uiOutLen)
 {
-
+	std::string sKey,sVal;
+	sKey.append(pKey);
+	reply r = c->run(command("GET") << sKey);
+	sVal = r.str();
+	uint32_t len = (unsigned) sVal.size();
+	if (len > uiOutLen)
+	{
+		return 0;
+	}
+	else 
+	{
+		std::copy(sVal.begin(), sVal.end(), pOut);
+		pOut[sVal.size()] = '\0';
+		std::cout << pOut << std::endl;
+		return len;
+	}
 
 }
 
 void RedisHelper::Put(char *pKey, uint32_t uiKeyLen, char *pVal, uint32_t uiValLen)
 {
+	std::string sKey,sVal;
+	sKey.append(pKey);
+	sVal.append(pVal);
+	c->run(command("SET") << sKey << sVal);
+	std::cout << uiKeyLen << " " << uiValLen << std::endl;
+	return;
 
 }
 
--- src/Test/testRedis.cpp
+++ src/Test/testRedis.cpp
@@ -16,13 +16,11 @@ void Close()
 
 uint32_t Get(char *pKey, uint32_t uiKeyLen, char *pOut, uint32_t uiOutLen)
 {
-	uint32_t len = 0;
 	std::string sKey,sVal;
 	sKey.append(pKey);
 	reply r = c->run(command("GET") << sKey);
 	sVal = r.str();
-
-	len = (unsigned) sVal.size();
+	uint32_t len = (unsigned) sVal.size();
 	if (len > uiOutLen)
 	{
 		return 0;
@@ -31,8 +29,6 @@ uint32_t Get(char *pKey, uint32_t uiKeyLen, char *pOut, uint32_t uiOutLen)
 	{
 		std::copy(sVal.begin(), sVal.end(), pOut);
 		pOut[sVal.size()] = '\0';
-	//	*pOut = const_cast<char*> (sVal.c_str());
-	//	std::cout << sVal.c_str() << std::endl;
 		std::cout << pOut << std::endl;
 		return len;
 	}
@@ -43,7 +39,6 @@ void Put(char *pKey, uint32_t uiKeyLen, char *pVal, uint32_t uiValLen)
 	std::string sKey,sVal;
 	sKey.append(pKey);
 	sVal.append(pVal);
-        //std::cout << sKey << " " << sVal;
 	c->run(command("SET") << sKey << sVal);
 	std::cout << uiKeyLen << " " << uiValLen << std::endl;
 	return;
@@ -52,8 +47,6 @@ void Put(char *pKey, uint32_t uiKeyLen, char *pVal, uint32_t uiValLen)
 
 int main(int argc, char **argv)
 {
-	//connection::ptr_t conn = connection::create();
-	//conn->run(command("SET") << "foo" << "bar" );
 	Open();
 	char* tkey = "Hi";
 	char* tvalue = "Redis";
@@ -61,8 +54,5 @@ int main(int argc, char **argv)
 	char* rvalue = new char[10];
 	uint32_t uiOutLen = 10;
 	std::cout << Get(tkey, 2, rvalue, uiOutLen) << std::endl;
-	//std::cout << *rvalue << std::endl;
-	// reply r = c->run(command("GET") << "Hello" );
-	// std::cout << "Hello is: " << r.str() << std::endl;
 
 }
