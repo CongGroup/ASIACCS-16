c3bd2e24e481ac9412ab63610aebec569612efc4
coolpenguin37 <coolpenguin37@gmail.com>
2015-10-15 22:14:47 +0800

    Modify testRedis.cpp

--- src/Test/testRedis.cpp
+++ src/Test/testRedis.cpp
@@ -3,10 +3,32 @@
 
 using namespace redis3m;
 
+void Open()
+{
+
+}
+
+void Close()
+{
+
+}
+
+uint32_t Get(char *pKey, uint32_t uiKeyLen, char *pOut, uint32_t uiOutLen)
+{
+
+
+}
+
+void Put(char *pKey, uint32_t uiKeyLen, char *pVal, uint32_t uiValLen)
+{
+}
+
+
 int main(int argc, char **argv)
 {
-        connection::ptr_t conn = connection::create();
-        conn->run(command("SET") << "foo" << "bar" );
-        reply r = conn->run(command("GET") << "foo" );        
-        std::cout << "FOO is: " << r.str() << std::endl;
+	connection::ptr_t conn = connection::create();
+	conn->run(command("SET") << "foo" << "bar" );
+	reply r = conn->run(command("GET") << "foo" );
+	std::cout << "FOO is: " << r.str() << std::endl;
+
 }
