16cfa15ce312b048573475f4b09c57a2529c005c
Raine Wang <rainewang@hotmail.com>
2015-10-28 23:08:54 +0800

    Change Client

--- src/Client_cpp/Ciphertext_throughput.cpp
+++ src/Client_cpp/Ciphertext_throughput.cpp
@@ -15,22 +15,24 @@ int main(int argc, char **argv)
 {
 	if (argc != 6)
 	{
-		cout << "usage : ./" << argv[0] << " [DataNodeNum] [A] [B] [C] [D]" << endl;
+		cout << "usage : ./" << argv[0] << " [DataNodeNum] [A] [B] [C] [D] [E]" << endl;
 		cout << "The test will begin at [A] ." << endl;
 		cout << "The test will consist [B] seconds" << endl;
 		cout << "The Key size is [C] ." << endl;
-		cout << "The Seed is [D] ." << endl;
+        cout << "0 for put. 1 for get. 2 for getCol" << endl;
+		cout << "The Seed is [E] ." << endl;
 		return 0;
 	}
 
 
 	//Get the params from command line
-	uint32_t uiBeg, uiTime, uiSeed, uiServerNum, uiKeyLen;
+	uint32_t uiBeg, uiTime, uiSeed, uiServerNum, uiKeyLen, uiOption;
 	sscanf(argv[1], "%u", &uiServerNum);
 	sscanf(argv[2], "%u", &uiBeg);
 	sscanf(argv[3], "%u", &uiTime);
 	sscanf(argv[4], "%u", &uiKeyLen);
-	sscanf(argv[5], "%u", &uiSeed);
+    sscanf(argv[5], "%u", &uiOption);
+	sscanf(argv[6], "%u", &uiSeed);
 
 	//Compute the params
 	uint32_t uiCurTime;
@@ -74,21 +76,31 @@ int main(int argc, char **argv)
 
 		(*pKeyCursorNum)++;
 
+        //Put
+        if (0 == uiOption)
+        {
 #ifdef DEF_INSERT_INDEX
-
-		client.Put("StudentScoreTable", strKey, "Score", (char*)strScore.c_str(), strScore.length(), true);
-
+            client.Put("StudentScoreTable", strKey, "Score", (char*)strScore.c_str(), strScore.length(), true);
 #else
-
-		client.Put("StudentScoreTable", strKey, "Score", (char*)strScore.c_str(), strScore.length(), false);
-
+            client.Put("StudentScoreTable", strKey, "Score", (char*)strScore.c_str(), strScore.length(), false);
 #endif
+        }
+        //Get
+        else if (1 == uiOption)
+        {
+            string stReturnScore;
+            client.Get(stReturnScore, "StudentScoreTable", strKey, "Score");
+        }
+        else if (2 == uiOption)
+        {
+            //GetCol
+        }
+        else
+        {
+            cout << "ERROR" << endl;
+            return 0;
+        }
 
-		for (int i = 0; i < 1; i++)
-		{
-			string stReturnScore;
-			client.Get(stReturnScore, "StudentScoreTable", strKey, "Score");
-		}
 
 		uiCnt++;
 	}
