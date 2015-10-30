3f7b0b9edad8749954fca3b8a5798d26647e6c42
Raine Wang <rainewang@hotmail.com>
2015-10-30 22:41:12 +0800

    Change 2

--- src/Client_cpp/Ciphertext_throughput.cpp
+++ src/Client_cpp/Ciphertext_throughput.cpp
@@ -13,12 +13,13 @@ using namespace caravel;
 
 int main(int argc, char **argv)
 {
-	if (argc != 7)
+	if (argc != 8)
 	{
-		cout << "usage : ./" << argv[0] << " [DataNodeNum] [A] [B] [C] [D] [E]" << endl;
+		cout << "usage : ./" << argv[0] << " [DataNodeNum] [A] [B] [C] [Size] [D] [E]" << endl;
 		cout << "The test will begin at [A] ." << endl;
 		cout << "The test will consist [B] seconds" << endl;
 		cout << "The Key size is [C] ." << endl;
+        cout << "The Value Size is [Size]" << endl;
         cout << "0 for put. 1 for get. 2 for getCol" << endl;
 		cout << "The Seed is [E] ." << endl;
 		return 0;
@@ -26,13 +27,14 @@ int main(int argc, char **argv)
 
 
 	//Get the params from command line
-	uint32_t uiBeg, uiTime, uiSeed, uiServerNum, uiKeyLen, uiOption;
+	uint32_t uiBeg, uiTime, uiSeed, uiServerNum, uiKeyLen, uiValLen, uiOption;
 	sscanf(argv[1], "%u", &uiServerNum);
 	sscanf(argv[2], "%u", &uiBeg);
 	sscanf(argv[3], "%u", &uiTime);
 	sscanf(argv[4], "%u", &uiKeyLen);
-    sscanf(argv[5], "%u", &uiOption);
-	sscanf(argv[6], "%u", &uiSeed);
+    sscanf(argv[5], "%u", &uiValLen);
+    sscanf(argv[6], "%u", &uiOption);
+	sscanf(argv[7], "%u", &uiSeed);
 
 	//Compute the params
 	uint32_t uiCurTime;
@@ -49,9 +51,9 @@ int main(int argc, char **argv)
 	pKeyCursorNum = (uint32_t*)(strKey.c_str());
 	*pKeyCursorNum = 0;
 
-	//Init strScore	
-	string strScore;
-	strScore.assign(128, 'A');
+	//Init strVal	
+	string strVal;
+    strVal.assign(uiValLen, 'A');
 
 	//Init the client to server
 	ClientCpp client;
@@ -80,9 +82,9 @@ int main(int argc, char **argv)
         if (0 == uiOption)
         {
 #ifdef DEF_INSERT_INDEX
-            client.Put("StudentScoreTable", strKey, "Score", (char*)strScore.c_str(), strScore.length(), true);
+            client.Put("StudentScoreTable", strKey, "Score", (char*)strVal.c_str(), strVal.length(), true);
 #else
-            client.Put("StudentScoreTable", strKey, "Score", (char*)strScore.c_str(), strScore.length(), false);
+            client.Put("StudentScoreTable", strKey, "Score", (char*)strVal.c_str(), strVal.length(), false);
 #endif
         }
         //Get
