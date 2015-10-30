89ea5d7e740bd2a82dc7e8928d4ab5a721f7082c
Raine Wang <rainewang@hotmail.com>
2015-10-30 22:30:42 +0800

    Change Ciphertext_latency

--- src/Client_cpp/Ciphertext_latency.cpp
+++ src/Client_cpp/Ciphertext_latency.cpp
@@ -11,31 +11,25 @@
 using namespace std;
 using namespace caravel;
 
-const int MAXRECORD = 1000000 + 100;
-const int iNum = 1000000;
-string strInput[MAXRECORD];
+
 
 int main(int argc, char **argv)
 {
-	if (argc != 7)
+	if (argc != 5)
 	{
-		cout << "usage : ./" << argv[0] << " [DataNodeNum] [A] [B] [C] [D] [E]" << endl;
-		cout << "The test will begin at [A] ." << endl;
-		cout << "The test will consist [B] seconds" << endl;
-		cout << "The Key size is [C] ." << endl;
-        cout << "0 for put. 1 for get. 2 for getCol" << endl;
-		cout << "The Seed is [E] ." << endl;
+		cout << "usage : ./" << argv[0] << " [DataNodeNum] [KeyLen] [ValLen] [Times]" << endl;
+		cout << "The Key size is [X] ." << endl;
+        cout << "The Value size is [X] ." << endl;
+        cout << "The num of times = [X]" << endl;
 		return 0;
 	}
 
 	//Get the params from command line
-	uint32_t uiBeg, uiTime, uiSeed, uiServerNum, uiKeyLen, uiOption;
+	uint32_t uiServerNum, uiKeyLen, uiValLen, uiOption, uiNum;
 	sscanf(argv[1], "%u", &uiServerNum);
-	sscanf(argv[2], "%u", &uiBeg);
-	sscanf(argv[3], "%u", &uiTime);
-	sscanf(argv[4], "%u", &uiKeyLen);
-    sscanf(argv[5], "%u", &uiOption);
-	sscanf(argv[6], "%u", &uiSeed);
+	sscanf(argv[2], "%u", &uiKeyLen);
+    sscanf(argv[2], "%u", &uiValLen);
+    sscanf(argv[4], "%u", &uiNum);
 
 	//Init the cache for key
 	string strKey;
@@ -47,9 +41,9 @@ int main(int argc, char **argv)
 	pKeyCursorNum = (uint32_t*)(strKey.c_str());
 	*pKeyCursorNum = 0;
 
-	//Init strScore	
-	string strScore;
-	strScore.assign(128, 'A');
+	//Init strVal	
+	string strVal;
+    strVal.assign(uiValLen, 'A');
 
 	//Init the client to server
 	ClientCpp client;
@@ -57,43 +51,69 @@ int main(int argc, char **argv)
 	client.InitExample(uiServerNum);
 	client.Open();
 
+    //For remember the time
 	uint32_t uiTimeDiff;
-	TimeDiff::DiffTimeInMicroSecond();
+
+    //init array for store keys
+    string *arStrInput = new string[uiNum];
+
+    //Init key pairs
+    for (uint32_t uiCur = 0; uiCur < uiNum; uiCur++)
+    {
+        (*pKeyCursorNum)++;
+        arStrInput[uiCur] = strKey;
+    }
+
+    TimeDiff::DiffTimeInMicroSecond();
 
 	//Test Put operation
-	for (int i = 0; i < iNum; ++i)
+	for (uint32_t uiCur = 0; uiCur < uiNum; uiCur++)
 	{
-		(*pKeyCursorNum)++;
-		strInput[i] = strKey;
+		
 #ifdef DEF_INSERT_INDEX
 
-		client.Put("StudentScoreTable", strKey, "Score", (char*)strScore.c_str(), strScore.length(), true);
+		client.Put("StudentScoreTable", arStrInput[uiCur], "Score", (char*)strVal.c_str(), strVal.length(), true);
 
 #else
 
-		client.Put("StudentScoreTable", strKey, "Score", (char*)strScore.c_str(), strScore.length(), false);
+		client.Put("StudentScoreTable", arStrInput[uiCur], "Score", (char*)strVal.c_str(), strVal.length(), false);
 
 #endif
 	}
 
 	uiTimeDiff = TimeDiff::DiffTimeInMicroSecond();
 
-	cout << "Putting " <<  iNum << " items cost time: " << uiTimeDiff << endl;
+	cout << "Putting " <<  uiNum << " items cost time: " << uiTimeDiff << endl;
 
 	//Test Get operation
-	string strVal;
 
 	TimeDiff::DiffTimeInMicroSecond();
-	for (int i = 0; i < iNum; ++i)
+
+    for (uint32_t uiCur = 0; uiCur < uiNum; uiCur++)
 	{
 		string stReturnScore;
-		client.Get(stReturnScore, "StudentScoreTable", strInput[i], "Score");
+		client.Get(stReturnScore, "StudentScoreTable", arStrInput[uiCur], "Score");
 	}
 
 	uiTimeDiff = TimeDiff::DiffTimeInMicroSecond();
-	cout << "Getting " <<  iNum << " items cost time: " << uiTimeDiff << endl;
+	cout << "Getting " <<  uiNum << " items cost time: " << uiTimeDiff << endl; 
+
+
+    //Test GetCol Operation
+
+    vector<string> vecResult;
+
+    TimeDiff::DiffTimeInMicroSecond();
+
+    client.GetCol(vecResult, "StudentScoreTable", "Score", 1000);
+
+    uiTimeDiff = TimeDiff::DiffTimeInMicroSecond();
+
+    cout << "Pull 1000 items from GetCol cost time: " << uiTimeDiff << endl;
 
 	cout << "-----------------------------------------------------------------------------" << endl;
 	cout << "Finish Test" << endl;
+
+    delete[] arStrInput;
 
 }
