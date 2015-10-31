8c23932bf425841292264d2001cb65b0c80e2c21
coolpenguin37 <coolpenguin37@gmail.com>
2015-10-31 14:12:49 +0800

    Update Parameters

--- src/Client_cpp/DemoConfig.h
+++ src/Client_cpp/DemoConfig.h
@@ -7,8 +7,8 @@
 
 #define DEMO_SECURITY_KEY "adfaksdfjklasdjflajsdiofjasodf"
 
-const std::string kDemoServerIP[] = { "10.4.0.5", "10.4.0.6" };
-const uint16_t kDemoServerPort[] = { 9090, 9090 };
+const std::string kDemoServerIP[] = { "10.4.0.5", "10.4.0.6", "10.4.0.9", "10.4.0.10", "10.4.0.11", "10.4.0.12", "10.4.0.13", "10.4.0.14", "10.4.0.15", "10.4.0.16" };
+const uint16_t kDemoServerPort[] = { 9090, 9090, 9090, 9090, 9090, 9090, 9090, 9090, 9090, 9090 };
 
 
 
--- src/Client_cpp/Plaintext_cluster_latency.cpp
+++ src/Client_cpp/Plaintext_cluster_latency.cpp
@@ -18,24 +18,25 @@ string strInput[MAXRECORD];
 int main(int argc, char **argv)
 {
 
-    if (argc != 6)
+    if (argc != 7)
     {
-        cout << "usage : ./" << argv[0] << " [DataNodeNum] [A] [B] [C] [D]" << endl;
+        cout << "usage : ./" << argv[0] << " [DataNodeNum] [A] [B] [C] [D] [E]" << endl;
         cout << "The test will begin at [A] ." << endl;
         cout << "The test will consist [B] seconds" << endl;
         cout << "The Key size is [C] ." << endl;
-        cout << "The Seed is [D] ." << endl;
+        cout << "0 for put. 1 for get. 2 for getCol" << endl;
+        cout << "The Seed is [E] ." << endl;
         return 0;
     }
 
-
     //Get the params from command line
-    uint32_t uiBeg, uiTime, uiSeed, uiServerNum, uiKeyLen;
+    uint32_t uiBeg, uiTime, uiSeed, uiServerNum, uiKeyLen, uiOption;
     sscanf(argv[1], "%u", &uiServerNum);
     sscanf(argv[2], "%u", &uiBeg);
     sscanf(argv[3], "%u", &uiTime);
     sscanf(argv[4], "%u", &uiKeyLen);
-    sscanf(argv[5], "%u", &uiSeed);
+    sscanf(argv[5], "%u", &uiOption);
+    sscanf(argv[6], "%u", &uiSeed);
 
 
     RedisHelper redisHelper;
